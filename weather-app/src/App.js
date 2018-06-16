c536ac19c68b29a91d8511a6804a58a9051ad9b7
Greg Thomas <webdeveloper@gregthomas.me>
2018-06-16 15:08:01 -0500

    connect to city api

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -3,32 +3,7 @@ import Home from './components/Home';
 import './App.css';
 
 class App extends Component {
-  componentDidMount() {
-  const cityInput = "new";
-  const url = 'https://andruxnet-world-cities-v1.p.mashape.com/?query=' + cityInput + '&searchby=city';
-
-  try {
-    fetch(url, {
-      method: 'GET',
-      headers: {
-      'X-Mashape-Key': 'elJSzs80GjmshE9NIhftugGztMYXp1OqX9qjsnBm6qPbeA8ktQ',
-      'Accept': 'application/json'
-      }
-    }).then(function(response) {
-      try {
-        return response.json();
-      } catch(error) {
-        console.log("response error - ", error);
-      }
-     
-    })
-    .then(function(myJson) {
-      console.log(myJson);
-    });
-  } catch(error) {
-    console.log('Error ', error);
-  }
-}
+  
 
   render() {
     return (
--- weather-app/src/components/Home.js
+++ weather-app/src/components/Home.js
@@ -1,9 +1,43 @@
 import React from 'react';
 
 class Home extends React.Component {
+	constructor() {
+		super();
+		this.state = {};
+	}
+
+
+	componentDidMount() {
+	  const cityInput = "new";
+	  const url = 'https://andruxnet-world-cities-v1.p.mashape.com/?query=' + cityInput + '&searchby=city';
+
+	  try {
+	    fetch(url, {
+	      method: 'GET',
+	      headers: {
+	      'X-Mashape-Key': 'elJSzs80GjmshE9NIhftugGztMYXp1OqX9qjsnBm6qPbeA8ktQ',
+	      'Accept': 'application/json'
+	      }
+	    }).then(function(response) {
+	      try {
+	        return response.json();
+	      } catch(error) {
+	        console.log("response error - ", error);
+	      }
+	     
+	    })
+	    .then(function(myJson) {
+	      console.log(myJson);
+	    });
+	  } catch(error) {
+	    console.log('Error ', error);
+	  }
+	}
 	render() {
 		return (
-			<h1>hello world</h1>
+			<div>
+				<input type="text" onChange={} />
+			</div>
 		);
 	}
 }
